afa6697a52b199a0819d537dadaedca11bd465d4
Sweta Jain <sweta.jain@neosoftmail.com>
2021-06-19 02:39:53 +0530

    auto

--- app/src/main/java/com/swetajain/viewpagerwithnavigation/StartFragmentOne.kt
+++ app/src/main/java/com/swetajain/viewpagerwithnavigation/StartFragmentOne.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.widget.ViewPager2
 import com.swetajain.viewpagerwithnavigation.databinding.FragmentStartOneBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * A simple [Fragment] subclass.
@@ -19,6 +23,11 @@ class StartFragmentOne : Fragment() {
     ): View {
         _binding = FragmentStartOneBinding.inflate(layoutInflater, container, false)
 
+        lifecycleScope.launch {
+            delay(1000)
+            val viewPager = activity?.findViewById<ViewPager2>(R.id.view_pager)
+            viewPager?.currentItem = 1
+        }
         return binding.root
     }
 
--- app/src/main/java/com/swetajain/viewpagerwithnavigation/StartFragmentTwo.kt
+++ app/src/main/java/com/swetajain/viewpagerwithnavigation/StartFragmentTwo.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.widget.ViewPager2
 import com.swetajain.viewpagerwithnavigation.databinding.FragmentStartTwoBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * A simple [Fragment] subclass.
@@ -18,6 +22,12 @@ class StartFragmentTwo : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentStartTwoBinding.inflate(layoutInflater, container, false)
+        lifecycleScope.launch {
+            delay(1000)
+            val viewPager = activity?.findViewById<ViewPager2>(R.id.view_pager)
+            viewPager?.currentItem = 2
+        }
+
         return binding.root
     }
 
